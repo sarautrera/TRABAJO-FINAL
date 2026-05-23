b0fcc25f38b6b0af2dc9617f0c60bec3d8daed4c
EstherLoechesM <estherloeches.m@gmail.com>
2026-05-23 20:33:46 +0200

    1: MiLinkedList

---
+++ src/es/uah/eedd/listas/prueba/Node.java
@@ -0,0 +1,14 @@
+package es.uah.eedd.listas.prueba;
+
+public class Node<T> {
+    T value;
+    Node<T> next;
+
+    //Creamos vagon, el NODO
+    Node(T value){
+        this.value = value;//Variable con la dirección del dato
+        this.next =null;//Variable con la direccion de otros nodos
+    }
+    //Si no ponemos ni public ni private las clases de un mismo paquete pueden acceder a sus variables con (.)
+    //Si pusieramos private entonces deberiamos crear los metodos getSiguiente() y setSitguiente()
+}
